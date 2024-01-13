e4f446ac9d672772cee55da7b94b980db26772d4
Neculai Marius Adrian <140186909+NeculaiMarius@users.noreply.github.com>
2024-01-13 18:18:11 +0200

    Update Proiect Bd.sql

--- Proiect Bd.sql
+++ Proiect Bd.sql
@@ -58,7 +58,7 @@ CREATE TABLE stocuri(
 
 --POPULARE TABELE
 
---Tabela ‘Furnizori’:
+--Tabela â€˜Furnizoriâ€™:
 INSERT INTO furnizori (id_furnizor, denumire_furnizor, adresa_email) VALUES (100, 'ProFurnizor', 'contact@profurnizor.com');
 INSERT INTO furnizori (id_furnizor, denumire_furnizor, adresa_email) VALUES (101, 'Global Suppliers', 'info@globalsuppliers.com');
 INSERT INTO furnizori (id_furnizor, denumire_furnizor, adresa_email) VALUES (102, 'Sunrise Traders', 'sales@sunrisetraders.com');
@@ -74,7 +74,7 @@ INSERT INTO furnizori (id_furnizor, denumire_furnizor, adresa_email) VALUES (109
 
 
 
-Tabela ‘Locatii_pr’:
+--Tabela â€˜Locatii_prâ€™:
 INSERT INTO locatii_pr (id_locatie, adresa, cod_postal, oras, judet) VALUES (1, 'Strada Victoriei, Nr. 10', '100100', 'Bucuresti', 'Bucuresti');
 INSERT INTO locatii_pr (id_locatie, adresa, cod_postal, oras, judet) VALUES (2, 'Bulevardul Republicii, Nr. 25', '200200', 'Cluj-Napoca', 'Cluj');
 INSERT INTO locatii_pr (id_locatie, adresa, cod_postal, oras, judet) VALUES (3, 'Strada Unirii, Nr. 15', '300300', 'Iasi', 'Iasi');
@@ -86,7 +86,7 @@ INSERT INTO locatii_pr (id_locatie, adresa, cod_postal, oras, judet) VALUES (8, 
 INSERT INTO locatii_pr (id_locatie, adresa, cod_postal, oras, judet) VALUES (9, 'Aleea Crangului, Nr. 9', '900900', 'Ploiesti', 'Prahova');
 INSERT INTO locatii_pr (id_locatie, adresa, cod_postal, oras, judet) VALUES (10, 'Bulevardul Nicolae Balcescu, Nr. 18', '101010', 'Craiova', 'Dolj');
 
-Tabela ‘Depozite:
+--Tabela â€˜Depozite:
 INSERT INTO depozite (id_depozit, id_furnizor, id_locatie, denumire_depozit, nume_responsabil, prenume_responsabil) VALUES (1001, 100, 1, 'Depozit Central', 'Neculai', 'Marius-Adrian');
 INSERT INTO depozite (id_depozit, id_furnizor, id_locatie, denumire_depozit, nume_responsabil, prenume_responsabil) VALUES (1002, 101, 2, 'Depozit Aurora', 'Ionescu', 'Mihai');
 INSERT INTO depozite (id_depozit, id_furnizor, id_locatie, denumire_depozit, nume_responsabil, prenume_responsabil) VALUES (1003, 102, 3, 'Depozit Meridian', 'Radulescu', 'Elena');
@@ -103,7 +103,7 @@ INSERT INTO depozite (id_depozit, id_furnizor, id_locatie, denumire_depozit, num
 INSERT INTO depozite (id_depozit, id_furnizor, id_locatie, denumire_depozit, nume_responsabil, prenume_responsabil) VALUES (1014, 103, 4, 'Depozit Terra', 'Dragomir', 'Ana');
 INSERT INTO depozite (id_depozit, id_furnizor, id_locatie, denumire_depozit, nume_responsabil, prenume_responsabil) VALUES (1015, 104, 5, 'Depozit Skyline', 'Vasilescu', 'Daniel');
 
-Tabela ‘Produse’:
+--Tabela â€˜Produseâ€™:
 INSERT ALL
     INTO produse_pr (id_produs, denumire_produs, categorie_produs)
     VALUES (1, 'Cafetier? automat?', 'Buc?t?rie')
@@ -172,12 +172,12 @@ INSERT ALL
     INTO produse_pr (id_produs, denumire_produs, categorie_produs)
     VALUES (33, 'Filtru de cafea permanent', 'Buc?t?rie')
     INTO produse_pr (id_produs, denumire_produs, categorie_produs)
-    VALUES (34, 'Aparat de înc?lzit cear? pentru epilat', 'Ingrijire personala')
+    VALUES (34, 'Aparat de Ã®nc?lzit cear? pentru epilat', 'Ingrijire personala')
     INTO produse_pr (id_produs, denumire_produs, categorie_produs)
     VALUES (35, 'Smart TV 4K', 'Electronice')
 SELECT * FROM DUAL;
 
-Tabela ‘Stocuri’:
+--Tabela â€˜Stocuriâ€™:
 INSERT INTO stocuri (id_stoc, id_depozit, id_produs, pret, cantitate, data_intrare_stoc) VALUES (1, 1001, 1, 120.50, 25, TO_DATE('2023-01-15', 'YYYY-MM-DD'));
 INSERT INTO stocuri (id_stoc, id_depozit, id_produs, pret, cantitate, data_intrare_stoc) VALUES (2, 1002, 2, 199.99, 15, TO_DATE('2023-02-20', 'YYYY-MM-DD'));
 INSERT INTO stocuri (id_stoc, id_depozit, id_produs, pret, cantitate, data_intrare_stoc) VALUES (3, 1003, 3, 85.00, 30, TO_DATE('2023-03-10', 'YYYY-MM-DD'));
@@ -213,7 +213,7 @@ WHERE id_depozit=1001;
 --INTEROGARI
 
 
---1.	S? se selecteze toate stocurile care au data de intrare în anul 2019.
+--1.	S? se selecteze toate stocurile care au data de intrare Ã®n anul 2019.
 SELECT * FROM stocuri
 WHERE EXTRACT(YEAR FROM data_intrare_stoc) = 2022;  
 
@@ -223,7 +223,7 @@ WHERE d.id_locatie=l.id_locatie
 AND UPPER(l.judet) like 'CLUJ';
  
 
---3.	S? se afiseze id-ul si denumirea produselor care se afl? în depozitul cu denumirea “Depozit Nexus”.
+--3.	S? se afiseze id-ul si denumirea produselor care se afl? Ã®n depozitul cu denumirea â€œDepozit Nexusâ€.
 SELECT p.id_produs, p.denumire_produs FROM produse_pr p, stocuri s, depozite d
 WHERE p.id_produs=s.id_produs
 AND d.id_depozit=s.id_depozit
@@ -236,14 +236,14 @@ FROM produse_pr
 GROUP BY categorie_produs;
  
 
---5.	S? se calculeze valoarea total? a produselor aflate în fiecare depozit.
+--5.	S? se calculeze valoarea total? a produselor aflate Ã®n fiecare depozit.
 SELECT d.id_depozit,d.denumire_depozit,sum(s.pret*s.cantitate) Valoare_TOTALA from produse_pr p,stocuri s,depozite d
 WHERE p.id_produs=s.id_produs
 AND d.id_depozit=s.id_depozit
 GROUP BY d.id_depozit,d.denumire_depozit;
 		 
 
---6.	S? se calculeze valoarea totala a produselor aflate în depozite, grupate în func?ie de ora?ul din care fac parte.
+--6.	S? se calculeze valoarea totala a produselor aflate Ã®n depozite, grupate Ã®n func?ie de ora?ul din care fac parte.
 SELECT l.oras, sum(s.pret*s.cantitate) Valoare_totala
 FROM locatii_pr l, depozite d, stocuri s, produse_pr p
 WHERE p.id_produs=s.id_produs
@@ -261,28 +261,28 @@ GROUP BY d.id_depozit, d.denumire_depozit, d.nume_responsabil
 HAVING sum(s.pret*s.cantitate)>4500;
  
 
---8.	S? se selecteze id-ul ?i denumire fiecarui produs care nu se afl? în nici unul din stocurile depozitelor.
+--8.	S? se selecteze id-ul ?i denumire fiecarui produs care nu se afl? Ã®n nici unul din stocurile depozitelor.
 SELECT p.id_produs,p.denumire_produs
 FROM produse_pr p,stocuri s
 WHERE p.id_produs=s.id_produs(+)
 AND id_stoc is null;
  
 
---9.	S? se modifice pre?ul produselor care sunt în stoc din anul 2020 astfel încât acesta s? scad? cu 10%.
+--9.	S? se modifice pre?ul produselor care sunt Ã®n stoc din anul 2020 astfel Ã®ncÃ¢t acesta s? scad? cu 10%.
 UPDATE stocuri
 SET pret = pret * 0.9 
 WHERE EXTRACT(YEAR FROM data_intrare_stoc) = 2020;
 
 
 
---10.	S? se modifice cantitate produselor care sunt în stoc ?i fac parte din categoria produselor de ‘GRADIN?RIT’ astfel încât aceasta s? scad? cu 5 unit??i.
+--10.	S? se modifice cantitate produselor care sunt Ã®n stoc ?i fac parte din categoria produselor de â€˜GRADIN?RITâ€™ astfel Ã®ncÃ¢t aceasta s? scad? cu 5 unit??i.
 UPDATE stocuri
 SET cantitate=cantitate-5
 WHERE stocuri.id_produs in (
 SELECT id_produs FROM produse_pr WHERE UPPER(categorie_produs) like 'GRADIN?RIT');
 
 
---11.	S? se selecteze toate produsele din depozitul care are denumirea ‘Zenith’ ?i a c?ror dat? de intrare în stoc este in luna martie. 
+--11.	S? se selecteze toate produsele din depozitul care are denumirea â€˜Zenithâ€™ ?i a c?ror dat? de intrare Ã®n stoc este in luna martie. 
 SELECT p.id_produs,p.denumire_produs,s.data_intrare_stoc 
 FROM stocuri s,produse_pr p,depozite d
 WHERE p.id_produs=s.id_produs
@@ -297,15 +297,15 @@ WHERE d.id_depozit=s.id_depozit
 AND sysdate - data_intrare_stoc < 200;
  
 
---13.	S? se selecteze toti furnizorii  a caror adresa de email incepe cu ‘info’.
+--13.	S? se selecteze toti furnizorii  a caror adresa de email incepe cu â€˜infoâ€™.
 SELECT * FROM furnizori 
 WHERE LOWER( SUBSTR (adresa_email, 0, 4)) ='info';
  
 
---14.	S? se determine diferit disponibilitatea fiec?rui produs prezet în toate depozitele în func?ie de cantitatea totala disponibila din acel produs astfel:
---  a.	Când cantitatea este mai mica de 20 de buc??i atunci disponibilitatea este ‘Stoc limitat’
---  b.	Când cantitatea este cuprins? între 20 ?i 35 de buc??i atunci disponibilitatea este ‘Stoc moderat’
---  c.	Când cantitatea este mai mare de 35 de buc??i atunci disponibilitatea este ‘Stoc suficient’
+--14.	S? se determine diferit disponibilitatea fiec?rui produs prezet Ã®n toate depozitele Ã®n func?ie de cantitatea totala disponibila din acel produs astfel:
+--  a.	CÃ¢nd cantitatea este mai mica de 20 de buc??i atunci disponibilitatea este â€˜Stoc limitatâ€™
+--  b.	CÃ¢nd cantitatea este cuprins? Ã®ntre 20 ?i 35 de buc??i atunci disponibilitatea este â€˜Stoc moderatâ€™
+--  c.	CÃ¢nd cantitatea este mai mare de 35 de buc??i atunci disponibilitatea este â€˜Stoc suficientâ€™
 --  S? se afiseze inregistrarile in ordine cresc?toare a cantit??ii totale.
 
 SELECT p.id_produs,p.denumire_produs,sum(s.cantitate),
@@ -319,13 +319,13 @@ WHERE p.id_produs=s.id_produs
 GROUP BY p.id_produs,p.denumire_produs
 ORDER BY SUM(s.cantitate);
  
---15.	S? se selecteze id-ul produsului, denumirea produsului ?i id-ul stocului din care face parte pentru toate produsele, chiar ?i cele care nu se afla în nici un stoc. Pentru produsele care nu se afla în nici un stoc, valoare NULL din coloana ‘id_stoc’ s? se înlocuiasc? cu cifra 0.
+--15.	S? se selecteze id-ul produsului, denumirea produsului ?i id-ul stocului din care face parte pentru toate produsele, chiar ?i cele care nu se afla Ã®n nici un stoc. Pentru produsele care nu se afla Ã®n nici un stoc, valoare NULL din coloana â€˜id_stocâ€™ s? se Ã®nlocuiasc? cu cifra 0.
 SELECT p.id_produs,p.denumire_produs,NVL(s.id_stoc,0)
 FROM produse_pr p,stocuri s
 WHERE p.id_produs=s.id_produs(+);
  
 
---16.	S? se afi?eze atât depozitele care au în stoc produse din categoria ‘GRADIN?RIT’ cât ?i depozitele care au în stoc produse din categoria ‘ACCESORII’.
+--16.	S? se afi?eze atÃ¢t depozitele care au Ã®n stoc produse din categoria â€˜GRADIN?RITâ€™ cÃ¢t ?i depozitele care au Ã®n stoc produse din categoria â€˜ACCESORIIâ€™.
 SELECT d.* FROM depozite d,stocuri s, produse_pr p
 WHERE d.id_depozit=s.id_depozit
 AND s.id_produs=p.id_produs
@@ -337,7 +337,7 @@ AND s.id_produs=p.id_produs
 AND UPPER(p.categorie_produs) LIKE 'GRADIN?RIT'; 
  
 
---17.	Sa se creeze o tabel? vitrual? care sa con?in? id-ul ?i denumire depozitelor, denumirea furnizorilor fiec?rui depozit, ora?ul, adresa ?i codul po?tal aferent fiec?rui depozit. Înregistr?rile s? se ordoneze dup? id-ul depozitelor.
+--17.	Sa se creeze o tabel? vitrual? care sa con?in? id-ul ?i denumire depozitelor, denumirea furnizorilor fiec?rui depozit, ora?ul, adresa ?i codul po?tal aferent fiec?rui depozit. ÃŽnregistr?rile s? se ordoneze dup? id-ul depozitelor.
 CREATE VIEW gestiune_depozite AS 
 SELECT d.id_depozit ,d.denumire_depozit,f.denumire_furnizor,l.oras,l.adresa,l.cod_postal
 FROM depozite d,locatii_pr l,furnizori f
@@ -348,7 +348,7 @@ ORDER BY d.id_depozit;
 SELECT * FROM gestiune_depozite;
  
 
---18.	S? se afi?eze toate produsele ?i cantit??ile disponibile în depozite mai pu?in cele care fac parte din categoria ‘Buc?t?rie’, ‘Gradin?rit’ ?i ’Accesorii’.
+--18.	S? se afi?eze toate produsele ?i cantit??ile disponibile Ã®n depozite mai pu?in cele care fac parte din categoria â€˜Buc?t?rieâ€™, â€˜Gradin?ritâ€™ ?i â€™Accesoriiâ€™.
 SELECT p.id_produs,p.denumire_produs,sum(s.cantitate)
 FROM produse_pr p,stocuri s
 WHERE p.id_produs=s.id_produs
@@ -361,8 +361,8 @@ AND (UPPER(p.categorie_produs)='BUC?T?RIE' or UPPER(p.categorie_produs)='GRADIN?
 GROUP BY p.id_produs,p.denumire_produs;
  
 
---19.	S? se introduc? un nou produs în tabela produse_pr cu id=99, denumire=’Periuta de dinti’, categorie=’Ingrijire personala’.
-S? se creeze un nou stoc în tabela stocuri cu id=99, pentru depozitul cu id=1001 care s? con?in? 10 buc??i din produsul cu id=99 la pre?ul de 25 lei ?i data de intrare în stoc s? fie data curent? a sistemului.
+--19.	S? se introduc? un nou produs Ã®n tabela produse_pr cu id=99, denumire=â€™Periuta de dintiâ€™, categorie=â€™Ingrijire personalaâ€™.
+S? se creeze un nou stoc Ã®n tabela stocuri cu id=99, pentru depozitul cu id=1001 care s? con?in? 10 buc??i din produsul cu id=99 la pre?ul de 25 lei ?i data de intrare Ã®n stoc s? fie data curent? a sistemului.
 
 INSERT INTO produse_pr (id_produs, denumire_produs, categorie_produs)
 VALUES (99, 'Periuta de dinti', 'Ingrijire personala');
@@ -382,7 +382,7 @@ AND s.id_produs=p.id_produs
 AND f.id_furnizor=100;
  
 
---21.	S? se selecteze loca?iile în care depozitele au mai mult de 40 de bucati de produse. Înregistr?rile s? se afiseze în ordine cresc?toare dupa num?rul de produse.
+--21.	S? se selecteze loca?iile Ã®n care depozitele au mai mult de 40 de bucati de produse. ÃŽnregistr?rile s? se afiseze Ã®n ordine cresc?toare dupa num?rul de produse.
 SELECT l.*,sum(s.cantitate) nr_buc_produse 
 FROM produse_pr p,depozite d,stocuri s,locatii_pr l
 WHERE l.id_locatie=d.id_locatie
@@ -393,7 +393,7 @@ HAVING sum(s.cantitate)>40
 ORDER BY sum(s.cantitate);
  
 
---22.	S? se ?tearg? iar apoi s? se recupereze tabela ‘locatii_pr’.
+--22.	S? se ?tearg? iar apoi s? se recupereze tabela â€˜locatii_prâ€™.
 DROP TABLE stocuri cascade constraints;
 FLASHBACK TABLE stocuri TO BEFORE DROP;
 
@@ -403,11 +403,11 @@ ADD CONSTRAINT fk_stocuri_d FOREIGN KEY(id_depozit) REFERENCES depozite(id_depoz
 ALTER TABLE stocuri
 ADD CONSTRAINT fk_stocuri_p FOREIGN KEY(id_produs) REFERENCES produse_pr(id_produs);
 
---23.	S? se creeze un sinonim pentru tabela locatii_pr cu denumirea ‘locatii_depozite’.
+--23.	S? se creeze un sinonim pentru tabela locatii_pr cu denumirea â€˜locatii_depoziteâ€™.
 CREATE SYNONYM locatii_depozite FOR locatii_pr;
 SELECT * FROM locatii_depozite; 
    
---24.	S? se creeze un raport în care s? se afiseze cate produse sunt în fiecare depozit, totalul cantitatilor de produse din fiecare depozit, si totalul pentru toate depozitele.
+--24.	S? se creeze un raport Ã®n care s? se afiseze cate produse sunt Ã®n fiecare depozit, totalul cantitatilor de produse din fiecare depozit, si totalul pentru toate depozitele.
 SELECT NVL(TO_CHAR(d.id_depozit),'Toate depozitele') Depozit,
     NVL(TO_CHAR(s.id_produs),'Total cantitate produse') Produs
     ,SUM(s.cantitate) cantitate ,LPAD(' ',5)||user as Utilizator
